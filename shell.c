5a376f79c7b306aa41438f7fc2a13bfb1a892758
mahlet <akotet2123@gmail.com>
2022-11-11 13:01:20 -0800

    eof

--- shell.c
+++ shell.c
@@ -2,32 +2,38 @@
 
 int main(int ac, char **av)
 {
+	int i = 0;
+	char *str;
+	char **tokens = NULL;
 	char *buffer = NULL;
 	size_t bufsize;
-
 
 	while (1)	
 	{
 		printf("#cisfun$ ");
-		if (getline (&buffer, &bufsize, stdin) == -1)
+		if (getline (&buffer, &bufsize, stdin) == EOF)
 		{
-			if (feof(stdin))
-				exit(EXIT_SUCCESS);
-			else
-			{
-				perror("./shell");
-				exit(0);
-			}
+			free(buffer);
+			exit(0);
 		}
-		/**str = strtok(buffer, *delim);
-		*av[0] = str;
-		*av[1] = NULL;
-		*/
-		av = _parse(buffer);
+		
+		tokens = malloc(sizeof(buffer) * 1024); 
+		if (tokens == NULL)
+			exit(0);
+
+		str = strtok(buffer, "\n ");
+		while (str != NULL)
+		{
+			tokens[i] = str;
+			str = strtok(NULL, "\n ");
+			i++;
+		}
+
+		/**av = _parse(buffer);*/
 			
 		if(fork() == 0)
 		{
-			if (execve(av[0], av, NULL) == -1)
+			if (execve(tokens[0], tokens, NULL) == -1)
 			{
 				perror("./shell");
 				exit(0);
@@ -39,6 +45,7 @@ int main(int ac, char **av)
 			wait(NULL);
 		}
 		free(buffer);
+		free(tokens)
 		buffer = NULL;
 	}	
 	return (0);
